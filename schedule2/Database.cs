fad8112c77f32d6468447cb23ed8db4fc6e9e93d
nmbaker <nmbaker@coe.edu>
2022-02-09 12:38:59 -0800

    test add

---
+++ schedule2/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace schedule2
+{
+    internal class Database
+    {
+        private FileStream db_file;
+        public Database()
+        {
+           db_file = File.Open("db.txt",FileMode.OpenOrCreate);
+        }
+
+        public CreateUser()
+    }
+}
