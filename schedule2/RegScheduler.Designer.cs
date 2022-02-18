1a6c458ca8e07296a02779aa25e606b46878f07a
jskacik <jskacik@SH308-18>
2022-02-18 14:49:16 -0600

    Got drag highlighting working

--- schedule2/RegScheduler.Designer.cs
+++ schedule2/RegScheduler.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -36,12 +38,24 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Gainsboro;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(191, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(478, 504);
+            this.dataGridView1.Size = new System.Drawing.Size(469, 603);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -49,7 +63,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(863, 516);
+            this.ClientSize = new System.Drawing.Size(863, 627);
             this.Controls.Add(this.dataGridView1);
             this.Name = "RegScheduler";
             this.Text = "RegScheduler";
--- schedule2/RegScheduler.cs
+++ schedule2/RegScheduler.cs
@@ -20,6 +20,7 @@ namespace schedule2
 
         private void RegScheduler_Load(object sender, EventArgs e)
         {
+            dataGridView1.SelectionChanged += new EventHandler(dataGridView1_SelectionChanged);
             dataGridView1.AutoGenerateColumns = false;
             dataGridView1.Columns.Add("Time", "Time");
             dataGridView1.Columns.Add("Monday", "Mon");
@@ -29,12 +30,54 @@ namespace schedule2
             dataGridView1.Columns.Add("Friday", "Fri");
             dataGridView1.Columns.Add("Saturday", "Sat");
             dataGridView1.Columns.Add("Sunday", "Sun");
+            String[] times = new String[] {"8:00am", "8:30am", "9:00am", "9:30am", "10:00am", "10:30am", "11:00am",
+             "11:30am", "12:00pm", "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm", "3:30pm", "4:00pm",
+             "4:30pm", "5:00pm", "5:30pm", "6:00pm", "6:30pm", "7:00pm", "7:30pm", "8:00pm", "8:30pm", "9:00pm", "9:30pm",
+             "10:00pm", "10:30pm", "11:00pm"};
+            for(int i = 0; i < times.Length; i++)
+            {
+                dataGridView1.Rows.Add(new object[] { times[i], "", "", "", "", "", "", "" });
+            }
 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dataGridView1.DefaultCellStyle.BackColor = Color.Gainsboro;
+            
+     
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void dataGridView1_SelectionChanged(Object sender, EventArgs e)
+        {
+            DataGridViewCellStyle unsel = new DataGridViewCellStyle();
+            DataGridViewCellStyle sel = new DataGridViewCellStyle();
+            unsel.BackColor = Color.Gainsboro;
+            sel.BackColor = Color.Crimson;
+
+            for (int i = 0; i < dataGridView1.SelectedCells.Count; i++)
+            {
+                if (Convert.ToString(dataGridView1.SelectedCells[i].Value) == "")
+                {
+                    if (dataGridView1.SelectedCells[i].Style.BackColor != sel.BackColor)
+                    {
+                        dataGridView1.SelectedCells[i].Style.BackColor = sel.BackColor;
+                        dataGridView1.ClearSelection();
+                    }
+                    else
+                    {
+                        dataGridView1.SelectedCells[i].Style.BackColor = unsel.BackColor;
+                        dataGridView1.ClearSelection();
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_MultiSelectChanged(Object sender, EventArgs e)
+        {
+            MessageBox.Show(" Selection Changed");
         }
     }
 }
