7ca8e65142e18a8d22d0d6d267b9faf688078dc3
jprichmond20 <78096963+jprichmond20@users.noreply.github.com>
2022-04-18 19:00:51 -0500

    Okayyyyy

---
+++ schedule2/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace schedule2
+{
+    public partial class Form2 : Form
+    {
+        public Director user;
+        public Form2(Director user)
+        {
+            InitializeComponent();
+            this.user = user;
+
+         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < progressBar1.Maximum)
+            {
+                progressBar1.Value += 1;
+            }
+            else
+            {
+                timer1.Stop();
+                var frm = new ScheduleView(user);
+                this.Hide();
+                frm.Location = this.Location;
+                frm.StartPosition = FormStartPosition.Manual;
+                frm.FormClosing += delegate { this.Close(); };
+                frm.Show();
+            }
+        }
+    }
+}
