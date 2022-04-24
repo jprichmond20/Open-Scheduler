225c5fc3d9c07f57a90742e6845f7e4e924211d0
jprichmond20 <78096963+jprichmond20@users.noreply.github.com>
2022-04-24 17:17:37 -0500

    reformat

--- schedule2/DirectorLanding.Designer.cs
+++ schedule2/DirectorLanding.Designer.cs
@@ -50,7 +50,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(42, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(666, 163);
+            this.pictureBox1.Size = new System.Drawing.Size(666, 126);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 24;
             this.pictureBox1.TabStop = false;
@@ -227,7 +227,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(750, 441);
+            this.ClientSize = new System.Drawing.Size(750, 416);
             this.Controls.Add(this.class16);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown1);
